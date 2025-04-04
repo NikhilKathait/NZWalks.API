31531aa04af7b309eb57f16a874f293ab3133afe
NIKHIL KATHAIT <nikhil.kathait@dxc.com>
2025-04-04 15:28:55 +0530

    EF Core and Create Walks API Controller

---
+++ NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    // /api/walks
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalksController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly IWalkRepository walkRepository;
+
+        public WalksController(IMapper mapper, IWalkRepository walkRepository)
+        {
+            this.mapper = mapper;
+            this.walkRepository = walkRepository;
+        }
+        // CREATE Walks
+        // POST: /api/walks
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddWalksRequestDto addWalksRequestDto)
+        {
+            // Map addWalksRequestDto DTO to walk Domain Model
+            var walkDomainModel = mapper.Map<Walk>(addWalksRequestDto);
+
+            await walkRepository.CreateAsync(walkDomainModel);
+
+            // Map Domain Model to DTO
+            return Ok(mapper.Map<WalkDto>(walkDomainModel));
+        }
+    }
+}
