8a2edc9ef694061bc212526960a9d10149351c3d
NIKHIL KATHAIT <nknick97@gmail.com>
2025-04-09 23:31:02 +0530

    Setting up Identity DbContext

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -18,6 +18,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<NZWalksDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnnectionString")));
 
+builder.Services.AddDbContext<NZWalksAuthDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksAuthConnnectionString")));
+
 builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
 builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
 
